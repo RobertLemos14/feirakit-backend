c36caf249dcd651f5c656c98b8659cb1aba25139
RobertLemos14 <robertlemos14@gmAIL.COM>
2023-04-12 20:05:04 -0300

    mudanças

--- src/program/database.py
+++ src/program/database.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
+import os
+import dotenv
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 class Database():
     def __init__(self):
-        self.client = MongoClient('link')
-        self.main = self.client['feirakit-database']
-        
+        self.client = MongoClient(os.getenv("DB_CONNECTION"))
+        self.main = self.client[os.getenv("DATABASE")]
+
 database = Database()
